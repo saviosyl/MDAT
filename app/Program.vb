d003961bc3c69230ae5936c3819bb4d56cfbcc8d
saviosyl <saviosyl@gmail.com>
2026-02-25 22:58:03 +0000

    Debug: show config state and license path check

--- app/Program.vb
+++ app/Program.vb
@@ -15,16 +15,26 @@ Module Program
         Application.EnableVisualStyles()
         Application.SetCompatibleTextRenderingDefault(False)
 
-        MessageBox.Show("DEBUG: Program.vb running. About to load config.", "DEBUG", MessageBoxButtons.OK, MessageBoxIcon.Information)
-
         ' Load config (for server URL and client token)
-        LoadConfig()
-
-        MessageBox.Show("DEBUG: Config loaded." & vbCrLf & "Server: " & SeatServerClient.ServerBaseUrl & vbCrLf & "Token: " & If(SeatServerClient.ClientToken.Length > 0, "SET (" & SeatServerClient.ClientToken.Length.ToString() & " chars)", "EMPTY"), "DEBUG", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Try
+            LoadConfig()
+        Catch ex As Exception
+            MessageBox.Show("Config load error: " & ex.Message, "DEBUG", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
 
         ' If no license.key exists, try to activate a trial automatically
         Dim licPath As String = Path.Combine(Application.StartupPath, LICENSE_FILE)
-        If Not File.Exists(licPath) Then
+        Dim licExists As Boolean = File.Exists(licPath)
+
+        MessageBox.Show( _
+            "DEBUG:" & vbCrLf & _
+            "Server: " & SeatServerClient.ServerBaseUrl & vbCrLf & _
+            "Token set: " & (SeatServerClient.ClientToken.Length > 0).ToString() & vbCrLf & _
+            "License path: " & licPath & vbCrLf & _
+            "License exists: " & licExists.ToString(), _
+            "DEBUG", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        If Not licExists Then
             TryActivateTrial(licPath)
         End If
 
